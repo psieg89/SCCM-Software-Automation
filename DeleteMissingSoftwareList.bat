4bb199263897a74015f34ab1c81b290f644292af
Peter <psieg89@gmail.com>
2015-03-25 09:37:13 -0400

    Create DeleteMissingSoftwareList.bat

---
+++ DeleteMissingSoftwareList.bat
@@ -0,0 +1,4 @@
+del /q "\\SCCMServer\C$\Collections\MissingSoftware\*.*"
+del /q "\\TeamShare$\MissingSoftware\*.*"
+del /q "\\SCCMServer\C$\Collections\MSList.txt"
+del /q "\\SCCMServer\C$\Collections\MissingSoftwareCollection.txt"
